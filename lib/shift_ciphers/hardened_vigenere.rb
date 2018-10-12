94978b403e04d29e62363c76bf032b8acdb6ec04
Tomasz Więch <tewu.dev@gmail.com>
2018-10-12 20:59:22 +0200

    Make HardenedVigenere offsets depend on already processed plaintext

--- lib/shift_ciphers/hardened_vigenere.rb
+++ lib/shift_ciphers/hardened_vigenere.rb
@@ -8,6 +8,28 @@ module ShiftCiphers
       @nonalphabet_char_strategy = nonalphabet_char_strategy
     end
 
+    protected
+
+    def process(text, encrypting = true)
+      offsets_stream = create_offsets_stream
+      plaintext_char = ""
+      text.each_char.reduce("") do |result, char|
+        char_idx = alphabet.index(char)
+        if !char_idx.nil?
+          rel_offset = offsets_stream.next(plaintext_char) * (encrypting ? 1 : -1)
+          result_char = alphabet[(char_idx + rel_offset) % alphabet.size]
+          plaintext_char = encrypting ? char : result_char
+          result << result_char
+        else
+          if nonalphabet_char_strategy == :dont_encrypt
+            result << char
+          else
+            raise CipherError.new("Invalid input #{text.inspect}. Character #{char.inspect} is not in the alphabet: #{alphabet.inspect}")
+          end
+        end
+      end
+    end
+
     def create_offsets_stream
       RandomOffsetsStream.new(key, alphabet.size - 1)
     end
@@ -21,7 +43,10 @@ module ShiftCiphers
         @max = max
       end
 
-      def next
+      def next(plaintext_char)
+        plaintext_char.bytes.each do |b|
+          @random = Random.new(@random.rand(@max) + b)
+        end
         @random.rand(@max)
       end
     end
