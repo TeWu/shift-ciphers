31f3ef61592ebe18ef180b33cbd60c56cb967d61
Tomasz Więch <tewu.dev@gmail.com>
2020-05-18 17:31:01 +0200

    Add Version::IS_STABLE

--- lib/shift_ciphers/version.rb
+++ lib/shift_ciphers/version.rb
@@ -4,7 +4,8 @@ module ShiftCiphers
     MINOR = 0
     PATCH = 0
     LABEL = nil
+    IS_STABLE = false
   end
 
-  VERSION = ([Version::MAJOR, Version::MINOR, Version::PATCH, Version::LABEL].compact * '.').freeze
+  VERSION = ([Version::MAJOR, Version::MINOR, Version::PATCH, Version::LABEL, Version::IS_STABLE ? nil : "next"].compact * '.').freeze
 end
