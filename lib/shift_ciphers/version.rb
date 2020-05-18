0f216e5c59e8d212c90491591e32e8473c8e005f
Tomasz Więch <tewu.dev@gmail.com>
2020-05-18 17:33:00 +0200

    Releasing 1.0.1

--- lib/shift_ciphers/version.rb
+++ lib/shift_ciphers/version.rb
@@ -2,9 +2,9 @@ module ShiftCiphers
   module Version
     MAJOR = 1
     MINOR = 0
-    PATCH = 0
+    PATCH = 1
     LABEL = nil
-    IS_STABLE = false
+    IS_STABLE = true
   end
 
   VERSION = ([Version::MAJOR, Version::MINOR, Version::PATCH, Version::LABEL, Version::IS_STABLE ? nil : "next"].compact * '.').freeze
